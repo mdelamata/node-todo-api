1e2be3cf97e9a7c1dada4ff44d703fe9f24d9ca0
Manuel M <mmatasgg@gmail.com>
2018-09-24 21:33:42 +0100

    Add test cases to DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -113,3 +113,42 @@ describe('GET /todos/:id', () => {
         .end(done);
   });
 });
+
+
+describe('DELETE /todos/:id', () => {
+
+    it('should remove a todo', (done) => {
+        var id = todos[0]._id.toHexString()
+
+        request(app)
+          .delete(`/todos/${id}`)
+          .expect(200)
+          .expect((res) => {
+            expect(res.body.todo._id).toBe(id);
+          })
+          .end((err, res) => {
+            if (err) {
+              return done(err);
+            }
+
+            Todo.findById(id).then((todo) => {
+              expect(todo).toBe(null);
+              done();
+            }).catch((e) => done(e));
+          });
+    });
+
+    it('should return a 404 if todo not found', (done) => {
+      request(app)
+        .delete(`/todos/${new ObjectID().toHexString()}}`)
+        .expect(404)
+        .end(done);
+    });
+
+    it('should return a 404 if id is not valid', (done) => {
+      request(app)
+        .delete(`/todos/12a`)
+        .expect(404)
+        .end(done);
+    });
+});
