1328cfb63dcb135ced0c3691bee80f3e29c492d8
Manuel M <mmatasgg@gmail.com>
2018-09-26 23:38:32 +0100

    Add authentication to GET /users/me

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -115,6 +116,9 @@ app.patch('/todos/:id', (req, res) => {
   });
 
 
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+});
 
 app.listen(port, ()=> {
   console.log(`>> Started on port ${port}`);
