0f173edcf59e0d388c3600788ba06d2dfcd0798e
Manuel M <mmatasgg@gmail.com>
2018-09-27 21:22:54 +0100

    Seed user test database.

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -1,6 +1,27 @@
 const {ObjectID} = require('mongodb');
+const jwt = require('jsonwebtoken');
 
 const {Todo} = require('./../../models/todo');
+const {User} = require('./../../models/user');
+
+const userOneId = new ObjectID()
+const userTwoId = new ObjectID()
+
+const users = [{
+  _id: userOneId,
+  email: 'manuel@example.com',
+  password: 'password1',
+  tokens: [{
+      access: 'auth',
+      token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
+  }]
+}, {
+  _id: userTwoId,
+  email: 'aunshi@example.com',
+  password: 'password2'
+}]
+
+
 
 
 const todos = [{
@@ -22,5 +43,13 @@ const populateTodos = (done) => {
   }).then(() => done());
 };
 
+const populateUsers = (done) => {
+  User.remove({}).then(() => {
+      var userOne = new User(users[0]).save();
+      var userTwo = new User(users[1]).save();
 
-module.exports = {todos, populateTodos};
+      return Promise.all([userOne, userTwo]);
+  }).then(() => done());
+};
+
+module.exports = {todos, populateTodos, users, populateUsers};
