7f4baa38f6c20c968a7f7fdc1bd5731e62611174
Manuel M <mmatasgg@gmail.com>
2018-09-29 09:28:15 +0100

    Add DELETE /users/me/token route.

--- server/server.js
+++ server/server.js
@@ -135,6 +135,14 @@ app.patch('/todos/:id', (req, res) => {
     });
 
 
+    app.delete('/users/me/token', authenticate, (req, res) => {
+      req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+      }).catch((e) => {
+        res.status(400).send();
+      });
+    });
+
 app.listen(port, ()=> {
   console.log(`>> Started on port ${port}`);
 })
